856f099f1300aec64cbfc89d61a8cce2d59036cd
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-04-20 17:07:15 -0500

    On branch master  Changes to be committed: 	modified:   indicators.py

--- indicators.py
+++ indicators.py
@@ -2,3 +2,43 @@ import ccxt
 import pandas as pd
 import numpy as np
 import talib
+
+exchange = ccxt.binanceus({
+    'apiKey': 'your-api-key',
+    'secret': 'your-api-secret',
+})
+
+# Fetch historical data
+symbol = 'RNDR/USD'
+timeframe = '1h'
+historical_data = exchange.fetch_ohlcv(symbol, timeframe)
+
+# Convert historical data to a pandas DataFrame
+columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
+df = pd.DataFrame(historical_data, columns=columns)
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+
+# Calculate technical indicators
+# MACD
+macd, signal, hist = talib.MACD(df['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+df['macd'] = macd
+df['macd_signal'] = signal
+df['macd_hist'] = hist
+
+# RSI
+rsi = talib.RSI(df['close'], timeperiod=14)
+df['rsi'] = rsi
+
+# SMA
+sma = talib.SMA(df['close'], timeperiod=20)
+df['sma'] = sma
+
+# Drop rows with missing values
+df.dropna(inplace=True)
+
+# Implement trading logic
+# Example: Buy when MACD histogram is positive and RSI is below 30, sell when MACD histogram is negative and RSI is above 70
+df['buy_signal'] = (df['macd_hist'] > 0) & (df['rsi'] < 30)
+df['sell_signal'] = (df['macd_hist'] < 0) & (df['rsi'] > 70)
+
+print(df.tail())
